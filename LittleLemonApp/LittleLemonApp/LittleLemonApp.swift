367fbeb6ac14dbe33399e90d0b8c2bfeab8a0b4b
Faizan Khan <108194702+jellyfishing2346@users.noreply.github.com>
2025-04-23 22:58:43 -0400

    Add files via upload
    Files for my food tracking app called Little Lemon

---
+++ LittleLemonApp/LittleLemonApp/LittleLemonApp.swift
@@ -0,0 +1,19 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+//
+//  Created by Faizan Khan
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    let persistenceController = PersistenceController.shared
+    
+    var body: some Scene {
+        WindowGroup {
+            Onboarding().environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
